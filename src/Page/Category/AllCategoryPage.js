f34a01a3e23fb841421c7ea35fa9081a6d2ddff6
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-28 20:59:53 +0300

    Add state from redux, page count and press Pagination in AllCategoryPage

--- src/Page/Category/AllCategoryPage.js
+++ src/Page/Category/AllCategoryPage.js
@@ -2,14 +2,37 @@ import React, { useEffect } from 'react'
 import CategoryContainer from '../../Components/Category/CategoryContainer'
 import Pagination from '../../Components/Uitily/Pagination'
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllCategory } from '../../Redux/Actions/CategoryAction';
+import { getAllCategory, getAllCategoryPage } from '../../Redux/Actions/CategoryAction';
 
 const AllCategoryPage = () => {
+
+    const dispatch = useDispatch();
+
+    // when first load
+    useEffect(() => {
+        dispatch(getAllCategory(4));
+    }, [])
+
+    // to get state from redux
+    const categoryData = useSelector(state => state.AllCategory.category)
+    const loading = useSelector(state => state.AllCategory.loading)
+    
+    // to get page count
+    let pageCount = 0 ;
+    if(categoryData.paginationResult) {
+        pageCount = categoryData.paginationResult.numberOfPages
+    }
+
+    // when press Pagination
+    const getPage=(page)=> {
+        dispatch(getAllCategoryPage(page))
+    }
     return (
         <div style={{minHeight:'670px'}}>
-        
-            <CategoryContainer />
-            <Pagination />
+            <CategoryContainer categoryData={categoryData.data} loading={loading} />
+            {
+                pageCount > 2 ? <Pagination pageCount={pageCount} onPress={getPage} /> : null
+            }
         </div>
     )
 }
