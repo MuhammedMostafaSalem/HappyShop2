8edbbb5f6d5629df05ce432c46ec437d9cf32473
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-28 20:57:23 +0300

    Add new (get all categoryPage) in categoryAction

--- src/Redux/Actions/CategoryAction.js
+++ src/Redux/Actions/CategoryAction.js
@@ -1,9 +1,23 @@
 import { GET_ALL_CATEGORY,  GET_ERROR } from "../type";
 import useGetData from "../../Hooks/useGetData";
 
-export const getAllCategory = () => async (dispatch) => {
+export const getAllCategory = (limit) => async (dispatch) => {
     try {
-        const res = await useGetData("/api/v1/categories");
+        const res = await useGetData(`/api/v1/categories?limit=${limit}`);
+        dispatch({
+            type: GET_ALL_CATEGORY,
+            payload: res,
+        })
+    }catch(err) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + err,
+        })
+    }
+}
+export const getAllCategoryPage = (page) => async (dispatch) => {
+    try {
+        const res = await useGetData(`/api/v1/categories?limit=4&page=${page}`);
         dispatch({
             type: GET_ALL_CATEGORY,
             payload: res,
