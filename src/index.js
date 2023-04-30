171470c03e9f750f9459bc57a5745c4bbc5b6e67
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-30 21:15:57 +0300

    Add ToastContainer in index.js file

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import Store from './Redux/Store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <Provider store={Store}>
+    <ToastContainer position="bottom-right" theme="colored" />
     <App />
   </Provider>,
   document.getElementById('root')
