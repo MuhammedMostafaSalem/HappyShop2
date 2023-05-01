3cbf4bc05de9d3d8cb33641a71921512dca645c7
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-05-01 18:04:07 +0300

    Update CategoryContainer

--- src/Components/Category/CategoryContainer.js
+++ src/Components/Category/CategoryContainer.js
@@ -1,13 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Container, Row, Spinner } from 'react-bootstrap'
 import CategoryCard from './../Category/CategoryCard';
-import clothe from "../../images/clothe.png";
-import cat2 from "../../images/cat2.png";
-import labtop from "../../images/labtop.png";
-import sale from "../../images/sale.png";
-import pic from "../../images/pic.png";
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllCategory } from '../../Redux/Actions/CategoryAction';
+
 const CategoryContainer = ({categoryData, loading}) => {
 
     const colors = ['#ffd3eb', '#f4dba5', '#55cfdf', '#ff6262', '#0034ff', '#ffd3e8']
