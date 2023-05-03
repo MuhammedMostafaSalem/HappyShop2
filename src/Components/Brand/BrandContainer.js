2bb662b73cd587fa54d78ce989d1941d7b94d8cc
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-05-03 17:56:23 +0300

    View Brand Page

--- src/Components/Brand/BrandContainer.js
+++ src/Components/Brand/BrandContainer.js
@@ -1,33 +1,22 @@
 import React from 'react'
 import BrandCard from './BrandCard'
-import brand1 from "../../images/brand1.png";
-import brand2 from "../../images/brand2.png";
-import brand3 from "../../images/brand3.png";
-import { Container, Row } from 'react-bootstrap';
-const BrandContainer = () => {
+import { Container, Row, Spinner } from 'react-bootstrap';
+const BrandContainer = ({ brandData, loading }) => {
     return (
         <Container>
             <div className="admin-content-text mt-2 ">كل الماركات</div>
             <Row className='my-1 d-flex justify-content-between'>
-                <BrandCard img={brand1} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand3} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand1} />
-                <BrandCard img={brand3} />
-                <BrandCard img={brand1} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand3} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand1} />
-                <BrandCard img={brand3} />
-                <BrandCard img={brand1} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand3} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand1} />
-                <BrandCard img={brand3} />
-
+                {
+                    loading === false ?
+                        brandData ?
+                            brandData.map((item, index)=> {
+                                return (
+                                    <BrandCard img={item.image} key={index} />
+                                )
+                            })
+                        : <h3>لا يوجد ماركات</h3>
+                    : <Spinner className='mx-auto' animation="border" variant="primary" />
+                }
             </Row>
         </Container>
     )
--- src/Page/Brand/AllBrandPage.js
+++ src/Page/Brand/AllBrandPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import BrandContainer from '../../Components/Brand/BrandContainer'
 import Pagination from '../../Components/Uitily/Pagination'
+import AllBrandPageHook from '../../CustomHook/Brand/AllBrandPageHook';
 const AllBrand = () => {
+
+    const [ brandData, loading, pageCount, getPage ] = AllBrandPageHook();
+
     return (
         <div style={{minHeight:'670px'}}>
-            <BrandContainer />
-            <Pagination />
+            <BrandContainer brandData={brandData.data} loading={loading} />
+            {
+                pageCount > 1 ? <Pagination pageCount={pageCount} onPress={getPage}/> : null
+            }
         </div>
     )
 }
