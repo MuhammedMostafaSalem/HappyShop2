59565cd58e3c76b1482909c0446fd4f3f92ad12d
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-05-03 22:06:56 +0300

    Admin Add SubCategory

--- src/Components/Admin/AdminAddSubCategory.js
+++ src/Components/Admin/AdminAddSubCategory.js
@@ -1,28 +1,44 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import AddSubCategoryhook from '../../CustomHook/SubCategory/AddSubCategoryhook'
 
 const AdminAddSubCategory = () => {
+
+    const [ id, name, categoryData, subCategory, handelChange, handelSubmit, onChangeName ] = AddSubCategoryhook();
+
+    if(categoryData) {
+        console.log(categoryData.data)
+    }
+
     return (
         <div>
-            <Row className="justify-content-start ">
+            <Row className="justify-content-start">
                 <div className="admin-content-text pb-4">اضافه تصنيف فرعي جديد</div>
                 <Col sm="8">
                     <input
                         type="text"
                         className="input-form d-block mt-3 px-3"
                         placeholder="اسم التصنيف الفرعي"
+                        onChange={onChangeName}
+                        value={name}
                     />
-                    <select name="languages" id="lang" className="select mt-3 px-2 ">
-                        <option value="val">التصنيف الاول</option>
-                        <option value="val2">التصنيف الثاني</option>
-                        <option value="val2">التصنيف الثالث</option>
-                        <option value="val2">التصنيف الرابع</option>
+                    <select name="category" id="cat" className="select mt-3 px-2" onChange={handelChange}>
+                        <option value="0">اختر تصنيف رئيسي</option>
+                        {
+                            categoryData.data ?
+                                categoryData.data.map((item)=> {
+                                    return (
+                                        <option value={item._id} key={item._id}>{item.name}</option>
+                                    )
+                                })
+                            : null
+                        }
                     </select>
                 </Col>
             </Row>
             <Row>
-                <Col sm="8" className="d-flex justify-content-end ">
-                    <button className="btn-save d-inline mt-2 ">حفظ التعديلات</button>
+                <Col sm="8" className="d-flex justify-content-end">
+                    <button className="btn-save d-inline mt-2" onClick={handelSubmit}>حفظ التعديلات</button>
                 </Col>
             </Row>
         </div>
