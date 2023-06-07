609c0df4f422f720e0aca7176afb2d90bfd0de79
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-07 10:26:46 +0300

    Update in AddProductsHook

--- src/CustomHook/Products/AddProductsHook.js
+++ src/CustomHook/Products/AddProductsHook.js
@@ -8,42 +8,18 @@ import { toast } from 'react-toastify';
 
 const AddProductsHook = () => {
 
-    // values images products
-    const [images, setImages] = useState({});
-    //values state
-    const [productName, setProductName] = useState('');
-    const [productDescription, setProductDescription] = useState('');
-    const [priceBefore, setPriceBefore] = useState('السعر قبل الخصم');
-    const [priceAfter, setPriceAfter] = useState('السعر بعد الخصم');
-    const [qty, setQty] = useState('الكمية المتاحة');
-    const [catID, setCatID] = useState('');
-    const [brandID, setBrandID] = useState('');
-    const [subCatID, setSubCatID] = useState([]);
-    const [seletedSubID, setSeletedSubID] = useState([]);
-    const [loading, setLoading] = useState(true);
-    //to show hide color picker
-    const [showColor, setShowColor] = useState(false);
-    //to store all pick color
-    const [colors, setColors] = useState([]);
-    const [options, setOptions] = useState([]);
-
-
-
-    const dispatch = useDispatch()
-
-    useEffect(()=> {
-        dispatch(getAllCategory())
-        dispatch(getAllBrand())
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getAllCategory());
+        dispatch(getAllBrand());
     }, [])
-
     //get last catgeory state from redux
     const category = useSelector(state => state.AllCategory.category)
     //get last brand state from redux
     const brand = useSelector(state => state.AllBrand.brand)
-    //get last sub-cat state from redux
+
+    //get last sub cat state from redux
     const subCat = useSelector(state => state.SubCategory.subcategory)
-
-
 
     const onSelect = (selectedList) => {
         setSeletedSubID(selectedList)
@@ -52,62 +28,56 @@ const AddProductsHook = () => {
         setSeletedSubID(selectedList)
     }
 
+    const [options, setOptions] = useState([]);
 
-    //when selet category store id
-    const onSeletCategory = async (e) => {
-        if(e.target.value !== 0){
-            await dispatch(getOneCategory(e.target.value))
-        }
-        setCatID(e.target.value)
-    }
-    useEffect(()=> {
-        if(catID !== 0) {
-            if(subCat.data) {
-                setOptions(subCat.data)
-            }
-        }
-    }, [catID])
+    //values images products
+    const [images, setImages] = useState({});
+    //values state
+    const [prodName, setProdName] = useState('');
+    const [prodDescription, setProdDescription] = useState('');
+    const [priceBefore, setPriceBefore] = useState('السعر قبل الخصم');
+    const [priceAftr, setPriceAftr] = useState('السعر بعد الخصم');
+    const [qty, setQty] = useState('الكمية المتاحة');
+    const [CatID, setCatID] = useState('');
+    const [BrandID, SetBrandID] = useState('');
+    const [subCatID, setSubCatID] = useState([]);
+    const [seletedSubID, setSeletedSubID] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    //when selet brand store id
-    const onSeletBrand = async (e) => {
-        setBrandID(e.target.value)
-    }
 
     //to change name state
-    const onChangeProductName = (event) => {
+    const onChangeProdName = (event) => {
         event.persist();
-        setProductName(event.target.value)
+        setProdName(event.target.value)
     }
-
-    //to change description  name state
+    //to change name state
     const onChangeDesName = (event) => {
         event.persist();
-        setProductDescription(event.target.value)
+        setProdDescription(event.target.value)
     }
-
-    //to change priceBefor state
+    //to change name state
     const onChangePriceBefor = (event) => {
         event.persist();
         setPriceBefore(event.target.value)
     }
-
-    //to change priceAfter state
+    //to change name state
     const onChangePriceAfter = (event) => {
         event.persist();
-        setPriceAfter(event.target.value)
-    }
-
-    //to change Qty state
+        setPriceAftr(event.target.value)
+    }  //to change name state
     const onChangeQty = (event) => {
         event.persist();
         setQty(event.target.value)
     }
-
-    //to change color state
     const onChangeColor = (event) => {
         event.persist();
         setShowColor(!showColor)
     }
+
+    //to show hide color picker
+    const [showColor, setShowColor] = useState(false);
+    //to store all pick color
+    const [colors, setColors] = useState([]);
     //when choose new color
     const handelChangeComplete = (color) => {
         setColors([...colors, color.hex])
@@ -119,48 +89,75 @@ const AddProductsHook = () => {
     }
 
 
-    //to convert base 64 to file
-    function dataURLtoFile(dataurl, filename) {
-        var arr = dataurl.split(','),
-            mime = arr[0].match(/:(.*?);/)[1],
-            bstr = atob(arr[arr.length - 1]), 
-            n = bstr.length, 
-            u8arr = new Uint8Array(n);
-        while(n--){
-            u8arr[n] = bstr.charCodeAt(n);
+
+    //when selet category store id
+    const onSeletCategory = async (e) => {
+        if (e.target.value !== 0) {
+            await dispatch(getOneCategory(e.target.value))
         }
-        return new File([u8arr], filename, {type:mime});
+        setCatID(e.target.value)
+    }
+    useEffect(() => {
+        if (CatID !== 0) {
+            if (subCat.data) {
+
+                setOptions(subCat.data)
+            }
+        }
+    }, [CatID])
+
+    //when selet brand store id
+    const onSeletBrand = (e) => {
+        SetBrandID(e.target.value)
     }
 
+    //to convert base 64 to file
+    function dataURLtoFile(dataurl, filename) {
 
-    // to save data
+        var arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+
+        return new File([u8arr], filename, { type: mime });
+    }
+
+    //to save data 
     const handelSubmit = async (e) => {
         e.preventDefault();
-
-        if(productName === "" || productDescription === "" || priceBefore <= 0 || images.length <= 0 || catID === 0) {
-            toast.warning('من فضلك اكمل البيانات')
+        if (CatID === 0 || prodName === "" || prodDescription === "" || images.length <= 0 || priceBefore <= 0) {
+            toast.warning("من فضلك اكمل البيانات")
             return;
         }
 
-        //convert base 64 image to file
-        const imgCover = dataURLtoFile(images[0] , Math.random() + ".png")
+        //convert base 64 image to file 
+        const imgCover = dataURLtoFile(images[0], Math.random() + ".png")
         //convert array of base 64 image to file 
-        const itemImages = Array.from(Array(Object.keys(images).length).keys()).map((item, index) => {
-            return dataURLtoFile(images[index] , Math.random() + ".png")
-        })
+        const itemImages = Array.from(Array(Object.keys(images).length).keys()).map(
+            (item, index) => {
+                return dataURLtoFile(images[index], Math.random() + ".png")
+            }
+        )
 
         const formData = new FormData();
-        formData.append("title", productName);
-        formData.append("description", productDescription);
-        formData.append("quantity", priceBefore);
-        formData.append("price", qty);
+        formData.append("title", prodName);
+        formData.append("description", prodDescription);
+        formData.append("quantity", qty);
+        formData.append("price", priceBefore);
         formData.append("imageCover", imgCover);
-        formData.append("category", catID);
-        formData.append("brand", brandID)
+        formData.append("category", CatID);
+        formData.append("brand", BrandID);
+        itemImages.map((item) => formData.append("images", item))
+
 
         colors.map((color) => formData.append("availableColors", color))
         seletedSubID.map((item) => formData.append("subcategory", item._id))
-        itemImages.map((item) => formData.append("images", item))
+
 
         setLoading(true)
         await dispatch(createProduct(formData))
@@ -170,63 +167,58 @@ const AddProductsHook = () => {
     //get create meesage
     const product = useSelector(state => state.ProductReducer.product)
 
-    useEffect(()=> {
-        if(loading === false) {
-            setCatID(0)
+    useEffect(() => {
+
+        if (loading === false) {
+           // setCatID(0)
             setColors([])
             setImages([])
-            setProductName('')
-            setProductDescription('')
+            setProdName('')
+            setProdDescription('')
             setPriceBefore('السعر قبل الخصم')
-            setPriceAfter('السعر بعد الخصم')
+            setPriceAftr('السعر بعد الخصم')
             setQty('الكمية المتاحة')
-            setBrandID(0)
+            SetBrandID(0)
             setSeletedSubID([])
-
-            setLoading(true)
+            setTimeout(() => setLoading(true), 1500)
 
             if (product) {
                 if (product.status === 201) {
-                    toast.success("تم عملية الاضافة بنجاح")
+                    toast.success("تم الاضافة بنجاح")
                 } else {
-                    toast.error("هناك مشكلة في عملية الاضافة")
+                    toast.error("هنالك مشكلة في عملية الاضافة")
                 }
             }
         }
-    },[loading])
+    }, [loading])
 
 
     return [
+        onChangeDesName,
+        onChangeQty,
+        onChangeColor,
+        onChangePriceAfter,
+        onChangePriceBefor,
+        onChangeProdName,
+        showColor,
+        category,
+        brand,
+        priceAftr,
         images,
         setImages,
-        productName,
-        setProductName,
-        productDescription,
-        setProductDescription,
-        priceBefore,
-        setPriceBefore,
-        priceAfter,
-        setPriceAfter,
-        qty,
-        setQty,
-        category,
-        onSeletCategory,
-        brand,
-        onSeletBrand,
-        showColor,
-        onChangeColor,
-        handelChangeComplete,
-        colors,
-        removeColor,
-        options,
         onSelect,
         onRemove,
+        options,
+        handelChangeComplete,
+        removeColor,
+        onSeletCategory,
         handelSubmit,
-        onChangeProductName,
-        onChangeDesName,
-        onChangePriceBefor,
-        onChangePriceAfter,
-        onChangeQty,
+        onSeletBrand,
+        colors,
+        priceBefore,
+        qty,
+        prodDescription,
+        prodName
     ]
 }
 
