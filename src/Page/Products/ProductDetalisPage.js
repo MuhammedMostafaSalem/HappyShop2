b36d6e45ff8057e7722079b582addbec99b013cb
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-09 05:05:16 +0300

    Add ViewProductsDetalisHook in ProductDetalisPage

--- src/Page/Products/ProductDetalisPage.js
+++ src/Page/Products/ProductDetalisPage.js
@@ -4,15 +4,26 @@ import CategoryHeader from '../../Components/Category/CategoryHeader'
 import CardProductsContainer from '../../Components/Products/CardProductsContainer'
 import ProductDetalis from '../../Components/Products/ProductDetalis'
 import RateContainer from '../../Components/Rate/RateContainer'
+import ViewProductsDetalisHook from '../../CustomHook/Products/ViewProductsDetalisHook'
+import { useParams } from 'react-router-dom'
 
 const ProductDetalisPage = () => {
+
+    const {id} = useParams();
+
+    const [item, images, cat, brand, prodLike] = ViewProductsDetalisHook(id)
+
+    if(prodLike) {
+        var items = prodLike.slice(0, 4)
+    }
+
     return (
         <div style={{ minHeight: '670px' }}>
             <CategoryHeader />
             <Container>
                 <ProductDetalis />
                 <RateContainer />
-                <CardProductsContainer title="منتجات قد تعجبك" />
+                <CardProductsContainer products={items} title="منتجات قد تعجبك" />
             </Container>
         </div>
     )
