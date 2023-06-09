939f87ab3b2d10ada2ac8be72c20a2bfcf62dd9e
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-09 05:03:18 +0300

    Create custon-hook ( ViewProductsDetalisHook )

---
+++ src/CustomHook/Products/ViewProductsDetalisHook.js
@@ -0,0 +1,91 @@
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getOneProduct, getProductLike } from '../../Redux/Actions/ProductAction';
+import mobile from '../../images/mobile.png'
+import { getOneCategory } from '../../Redux/Actions/CategoryAction';
+import { getOneBrand } from '../../Redux/Actions/BrandAction';
+
+const ViewProductsDetalisHook = (prodID) => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getOneProduct(prodID))
+    }, [])
+    
+    const oneProduct = useSelector((state) => state.ProductReducer.oneProduct)
+    const oneCategory = useSelector((state) => state.AllCategory.oneCategory)
+    const oneBrand = useSelector((state) => state.AllBrand.oneBrand)
+    const productLike = useSelector((state) => state.ProductReducer.productLike)
+
+    // to show product item
+    let item = []
+    if(oneProduct.data) {
+        item = oneProduct.data;
+    }
+    else {
+        item = []
+    }
+
+
+
+    useEffect(() => {
+        if(item.category) {
+            dispatch(getOneCategory(item.category))
+        }
+        if(item.brand) {
+            dispatch(getOneBrand(item.brand))
+        }
+        if(item.category) {
+            dispatch(getProductLike(item.category))
+        }
+    }, [item])
+
+
+
+    // to view images gallery
+    let images = []
+    if(item.images) {
+        images = item.images.map((img) => {
+            return {original: img}
+        })
+    }
+    else {
+        images = [
+            {
+                original: `${mobile}`,
+            },
+        ]
+    }
+
+    // to show cat item
+    let cat = []
+    if(oneCategory.data) {
+        cat = oneCategory.data;
+    }
+    else {
+        cat = []
+    }
+
+    // to show brand item
+    let brand = []
+    if(oneBrand.data) {
+        brand = oneBrand.data;
+    }
+    else {
+        brand = []
+    }
+
+    // to show productLike item
+    let prodLike = []
+    if (productLike) {
+        prodLike = productLike.data;
+    }
+    else {
+        prodLike = []
+    }
+
+    return [item, images, cat, brand, prodLike]
+}
+
+export default ViewProductsDetalisHook
