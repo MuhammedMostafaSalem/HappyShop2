dae9a64ec2b66915af4da505005d4600a07d2376
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-09 05:04:01 +0300

    Add ViewProductsDetalisHook in ProductGallery

--- src/Components/Products/ProductGallery.js
+++ src/Components/Products/ProductGallery.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
-import mobile from '../../images/mobile.png'
 import LeftButton from './LeftButton';
 import RightButton from './RightButton';
+import { useParams } from 'react-router-dom';
+import ViewProductsDetalisHook from './../../CustomHook/Products/ViewProductsDetalisHook';
 const ProductGallery = () => {
-    const images = [
-        {
-            original: `${mobile}`,
-        },
-        {
-            original: `${mobile}`,
-        },
-        {
-            original: `${mobile}`,
-        },
-    ];
+
+    const {id} = useParams();
+
+    const [item, images, cat] = ViewProductsDetalisHook(id);
+
     return (
         <div className="product-gallary-card d-flex justfiy-content-center  align-items-center
         pt-2">
             <ImageGallery items={images}
-                defaultImage={mobile}
                 showFullscreenButton={false}
                 isRTL={true}
                 showPlayButton={false}
