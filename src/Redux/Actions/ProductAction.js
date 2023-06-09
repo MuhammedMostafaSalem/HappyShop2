ffc9cff8bda92b34649f699b9f02701f69efbfad
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-09 04:58:08 +0300

    Add getAllProductsPage, getOneProduct and getProductLike in ProductAction

--- src/Redux/Actions/ProductAction.js
+++ src/Redux/Actions/ProductAction.js
@@ -1,6 +1,6 @@
 import useGetData from "../../Hooks/useGetData";
 import { useInsertDataWithImg } from "../../Hooks/useInsertData";
-import { CREATE_PRODUCT, GET_ALL_PRODUCT, GET_ERROR } from "../type";
+import { CREATE_PRODUCT, GET_ALL_PRODUCT, GET_ERROR, GET_PRODUCT_DETAILS, GET_PRODUCT_LIKE } from "../type";
 
 //create products with pagination
 export const createProduct = (formData) => async (dispatch) => {
@@ -26,6 +26,60 @@ export const getAllProducts = () => async (dispatch) => {
         const res = await useGetData(`/api/v1/products`);
         dispatch({
             type: GET_ALL_PRODUCT,
+            payload: res,
+            loading: true,
+        })
+    }catch(err) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + err,
+        })
+    }
+}
+
+
+//get all products with pagination with pages number
+export const getAllProductsPage = (page, limit) => async (dispatch) => {
+    try {
+        const res = await useGetData(`/api/v1/products?page=${page}&limit=${limit}`);
+        dispatch({
+            type: GET_ALL_PRODUCT,
+            payload: res,
+            loading: true,
+        })
+    }catch(err) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + err,
+        })
+    }
+}
+
+
+//get one product with id
+export const getOneProduct = (id) => async (dispatch) => {
+    try {
+        const res = await useGetData(`/api/v1/products/${id}`);
+        dispatch({
+            type: GET_PRODUCT_DETAILS,
+            payload: res,
+            loading: true,
+        })
+    }catch(err) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + err,
+        })
+    }
+}
+
+
+//get one product with id
+export const getProductLike = (id) => async (dispatch) => {
+    try {
+        const res = await useGetData(`/api/v1/products?category=${id}`);
+        dispatch({
+            type: GET_PRODUCT_LIKE,
             payload: res,
             loading: true,
         })
