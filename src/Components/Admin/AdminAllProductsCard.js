a33f1e9eeae71f16bc2b503178e04fd7f2273c69
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-10 11:21:05 +0300

    Add delete admin product in AdminAllProductsCard

--- src/Components/Admin/AdminAllProductsCard.js
+++ src/Components/Admin/AdminAllProductsCard.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col,Card,Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import prod1 from '../../images/prod1.png'
-const AdminAllProductsCard = () => {
+import { Modal, Button } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+import { deleteProducts } from '../../Redux/Actions/ProductAction'
+import { toast } from 'react-toastify'
+const AdminAllProductsCard = ({item}) => {
+
+    // delete admin product
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const dispatch = useDispatch();
+
+    const handleDelete = async () => {
+        await dispatch(deleteProducts(item._id))
+        toast.success('تمت عملية الازالة بنجاح')
+        setShow(false);
+        window.location.reload()
+    }
+
     return (
         <Col xs="12" sm="6" md="5" lg="4" className="d-flex">
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>تأكيد الازالة</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>هل انت متأكد من عملية الازالة ؟</Modal.Body>
+                <Modal.Footer style={{borderTop: "none"}}>
+                    <Button variant="danger" onClick={handleClose}>
+                        اغلاق
+                    </Button>
+                    <Button variant="secondary" onClick={handleDelete}>
+                        ازالة
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <Card
                 className="my-2"
                 style={{
@@ -16,24 +52,24 @@ const AdminAllProductsCard = () => {
                 }}>
                 <Row className="d-flex justify-content-center px-2">
                     <Col className=" d-flex justify-content-between">
-                        <div className="d-inline item-delete-edit">ازاله</div>
+                        <div onClick={handleShow} className="d-inline item-delete-edit">ازاله</div>
                         <div className="d-inline item-delete-edit">تعديل</div>
                     </Col>
                 </Row>
-                <Link to="/products/:id" style={{ textDecoration: "none" }}>
-                    <Card.Img style={{ height: "228px", width: "100%" }} src={prod1} />
+                <Link to={`/products/${item._id}`} style={{ textDecoration: "none" }}>
+                    <Card.Img style={{ height: "228px", width: "100%" }} src={item.imageCover} />
                     <Card.Body>
                         <Card.Title>
                             <div className="card-title">
-                                سود كربون ساعة يد ذكية بيب إس أسود كربون{" "}
+                                {item.title}
                             </div>
                         </Card.Title>
                         <Card.Text>
                             <div className="d-flex justify-content-between">
-                                <div className="card-rate">4.5</div>
+                                <div className="card-rate">{item.ratingsQuantity}</div>
                                 <div className="d-flex">
                                     <div className="card-currency mx-1">جنيه</div>
-                                    <div className="card-price">880</div>
+                                    <div className="card-price">{item.price}</div>
                                 </div>
                             </div>
                         </Card.Text>
