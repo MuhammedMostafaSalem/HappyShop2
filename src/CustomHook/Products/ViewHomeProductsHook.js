ac29280b7baea3507ff069977cb60eed76bac276
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-19 02:29:14 +0300

    Update in ViewHomeProductsHook

--- src/CustomHook/Products/ViewHomeProductsHook.js
+++ src/CustomHook/Products/ViewHomeProductsHook.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import {useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllProducts } from '../../Redux/Actions/ProductAction';
 
@@ -13,12 +13,15 @@ const ViewHomeProductsHook = () => {
     const allProduct = useSelector((state) => state.ProductReducer.allProduct)
 
     let items = []
-    if(allProduct.data) {
-        items = allProduct.data.slice(0, 4);
-    }
-    else {
-        items = []
-    }
+
+    try {
+        if(allProduct.data) {
+            items = allProduct.data.slice(0, 4);
+        }
+        else {
+            items = []
+        }
+    } catch(e) {}
 
     return [items]
 }
