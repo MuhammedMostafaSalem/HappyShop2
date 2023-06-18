aea6bc73de76a3959adce895172b1dfb77396753
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-19 02:34:59 +0300

    Update in AdminAllProductsCard

--- src/Components/Admin/AdminAllProductsCard.js
+++ src/Components/Admin/AdminAllProductsCard.js
@@ -53,7 +53,9 @@ const AdminAllProductsCard = ({item}) => {
                 <Row className="d-flex justify-content-center px-2">
                     <Col className=" d-flex justify-content-between">
                         <div onClick={handleShow} className="d-inline item-delete-edit">ازاله</div>
-                        <div className="d-inline item-delete-edit">تعديل</div>
+                        <Link to={`/admin/edit-product/${item._id}`} style={{ textDecoration: "none" }}>
+                            <div className="d-inline item-delete-edit">تعديل</div>
+                        </Link>
                     </Col>
                 </Row>
                 <Link to={`/products/${item._id}`} style={{ textDecoration: "none" }}>
