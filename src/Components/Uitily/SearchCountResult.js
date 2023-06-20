94338b4f795fb60fce363b5280b53e45af19d49a
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-20 05:36:15 +0300

    Add event ( clickMe ) for sort product in SearchCountResult component

--- src/Components/Uitily/SearchCountResult.js
+++ src/Components/Uitily/SearchCountResult.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import UnopDropdown from "unop-react-dropdown";
 import sort from '../../images/sort.png'
-const SearchCountResult = ({title}) => {
+const SearchCountResult = ({title, getProduct}) => {
     const handler=()=> {
 
     }
+
+    const clickMe=(key)=> {
+        localStorage.setItem('sortType', key)
+        getProduct();
+    }
+    
     return (
         <div className="d-flex justify-content-between pt-3 px-2">
             <div className="sub-tile">{title}</div>
@@ -28,12 +34,11 @@ const SearchCountResult = ({title}) => {
                     align="CENTER"
                     hover>
                     <div className="card-filter">
-                        <div className="border-bottom card-filter-item">الاكثر مبيعا</div>
-                        <div className="border-bottom card-filter-item">الاعلي تقييما</div>
-                        <div className="border-bottom card-filter-item">
-                            السعر من الاقل للاعلي
-                        </div>
-                        <div className=" card-filter-item">السعر من الاعلي للاقل</div>
+                        <div onClick={() => clickMe('')} className="border-bottom card-filter-item">بدون ترتيب</div>
+                        <div onClick={() => clickMe('الاكثر مبيعا')} className="border-bottom card-filter-item">الاكثر مبيعا</div>
+                        <div onClick={() => clickMe('الاعلي تقييما')} className="border-bottom card-filter-item">الاعلي تقييما</div>
+                        <div onClick={() => clickMe('السعر من الاقل للاعلي')} className="border-bottom card-filter-item">السعر من الاقل للاعلي</div>
+                        <div onClick={() => clickMe('السعر من الاعلي للاقل')} className=" card-filter-item">السعر من الاعلي للاقل</div>
                     </div>
                 </UnopDropdown>
             </div>
