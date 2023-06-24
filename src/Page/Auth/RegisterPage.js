122af5b408abb3905d9837a8a2a6898200bcb492
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-24 03:30:09 +0300

    Add RegisterHook in RegisterPage

--- src/Page/Auth/RegisterPage.js
+++ src/Page/Auth/RegisterPage.js
@@ -1,29 +1,52 @@
 import React from 'react'
 import { Container,Row,Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import RegisterHook from '../../CustomHook/Auth/RegisterHook'
 
 const RegisterPage = () => {
+  const [ name, eMail, phone, password, confirmPassword, loading, onChangeName, onChangeEmail, onChangePhone, onChangePassword, onChangeConfirmPassword, onSubmit ] = RegisterHook();
+  
     return (
         <Container style={{ minHeight: "680px" }}>
         <Row className="py-5 d-flex justify-content-center hieght-search">
           <Col sm="12" className="d-flex flex-column ">
             <label className="mx-auto title-login">تسجيل حساب جديد</label>
             <input
+              value={name}
+              onChange={onChangeName}
               placeholder="اسم المستخدم..."
               type="text"
               className="user-input mt-3 text-center mx-auto"
             />
             <input
+              value={eMail}
+              onChange={onChangeEmail}
               placeholder="الايميل..."
               type="text"
-              className="user-input my-3 text-center mx-auto"
+              className="user-input mt-3 text-center mx-auto"
             />
             <input
+              value={phone}
+              onChange={onChangePhone}
+              placeholder="الهاتف..."
+              type="phone"
+              className="user-input mt-3 text-center mx-auto"
+            />
+            <input
+              value={password}
+              onChange={onChangePassword}
               placeholder="كلمه السر..."
               type="password"
-              className="user-input text-center mx-auto"
+              className="user-input mt-3 text-center mx-auto"
             />
-            <button className="btn-login mx-auto mt-4">تسجيل الحساب</button>
+            <input
+              value={confirmPassword}
+              onChange={onChangeConfirmPassword}
+              placeholder="تأكيد كلمة السر..."
+              type="password"
+              className="user-input mt-3 text-center mx-auto"
+            />
+            <button onClick={onSubmit} className="btn-login mx-auto mt-4">تسجيل الحساب</button>
             <label className="mx-auto my-4">
               لديك حساب بالفعل؟{" "}
               <Link to="/login" style={{ textDecoration: "none" }}>
