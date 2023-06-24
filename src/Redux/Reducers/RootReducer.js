8052fdb1883200b5a7b2faa05697a943e39868c0
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-24 03:28:38 +0300

    Add AuthReducer in rootReducer

--- src/Redux/Reducers/RootReducer.js
+++ src/Redux/Reducers/RootReducer.js
@@ -3,10 +3,12 @@ import CategoryReducer from "./CategoryReducer";
 import BrandReducer from "./BrandReducer";
 import SubCategoryReducer from "./SubCategoryReducer";
 import ProductReducer from "./ProductReducer";
+import AuthReducer from "./AuthReducer";
 
 export default combineReducers({
     AllCategory: CategoryReducer,
     AllBrand: BrandReducer,
     SubCategory: SubCategoryReducer,
-    ProductReducer: ProductReducer
+    ProductReducer: ProductReducer,
+    AuthReducer: AuthReducer
 })
