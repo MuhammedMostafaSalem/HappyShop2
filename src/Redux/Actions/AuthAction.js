e136f5629922d9c2ed49dd61b2900cf359c97bb1
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-29 13:04:49 +0300

    Create loginUser and getLoggedUser in AuthAction

--- src/Redux/Actions/AuthAction.js
+++ src/Redux/Actions/AuthAction.js
@@ -1,5 +1,6 @@
+import { useGetDataToken } from "../../Hooks/useGetData";
 import { useInsertData } from "../../Hooks/useInsertData";
-import { CREATE_NEW_USER } from "../type";
+import { CREATE_NEW_USER, GET_CURERNT_USER, LOGIN_USER } from "../type";
 
 // create new user
 export const createNewUser = (data) => async (dispatch) => {
@@ -14,6 +15,42 @@ export const createNewUser = (data) => async (dispatch) => {
     } catch (e) {
         dispatch({
             type: CREATE_NEW_USER,
+            payload: e.response,
+        })
+    }
+}
+
+// login user
+export const loginUser = (data) => async (dispatch) => {
+    try {
+        const response = await useInsertData(`/api/v1/auth/login`, data);
+        dispatch({
+            type: LOGIN_USER,
+            payload: response,
+            loading: true
+        })
+
+    } catch (e) {
+        dispatch({
+            type: LOGIN_USER,
+            payload: e.response,
+        })
+    }
+}
+
+//login  user 
+export const getLoggedUser = () => async (dispatch) => {
+    try {
+        const response = await useGetDataToken(`/api/v1/users/getMe`);
+        dispatch({
+            type: GET_CURERNT_USER,
+            payload: response,
+            loading: true
+        })
+
+    } catch (e) {
+        dispatch({
+            type: GET_CURERNT_USER,
             payload: e.response,
         })
     }
