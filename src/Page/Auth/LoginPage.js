279d808dfb5d2445180d91e16b88866e58e151ae
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-06-29 13:08:36 +0300

    Add LoginHook in LoginPage

--- src/Page/Auth/LoginPage.js
+++ src/Page/Auth/LoginPage.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import LoginHook from '../../CustomHook/Auth/LoginHook'
 
 const LoginPage = () => {
+    const [email, password, loading, onChangeEmail, onChangePassword, onSubmit, isPress] = LoginHook();
+
     return (
             <Container style={{ minHeight: "680px" }}>
                 <Row className="py-5 d-flex justify-content-center ">
                     <Col sm="12" className="d-flex flex-column ">
                         <label className="mx-auto title-login">تسجيل الدخول</label>
                         <input
+                            value={email}
+                            onChange={onChangeEmail}
                             placeholder="الايميل..."
-                            type="text"
+                            type="email"
                             className="user-input my-3 text-center mx-auto"
                         />
                         <input
+                            value={password}
+                            onChange={onChangePassword}
                             placeholder="كلمه السر..."
                             type="password"
                             className="user-input text-center mx-auto"
                         />
-                        <button className="btn-login mx-auto mt-4">تسجيل الدخول</button>
+                        <button className="btn-login mx-auto mt-4" onClick={onSubmit}>تسجيل الدخول</button>
                         <label className="mx-auto my-4">
                             ليس لديك حساب ؟{" "}
                             <Link to="/register" style={{textDecoration:'none'}}>
@@ -28,7 +35,13 @@ const LoginPage = () => {
                             </Link>
                         </label>
 
-
+                        {
+                            isPress === true ?
+                                loading === true ?
+                                    <Spinner className='mx-auto' animation="border" variant="primary" />
+                                : null
+                            : null
+                        }
 
                     </Col>
 
