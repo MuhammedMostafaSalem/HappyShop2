ed68108453fd625ed54bfdf5784128ed8e1571a2
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-18 16:10:38 +0300

    Add AddRateHook in RatePost component

--- src/Components/Rate/RatePost.js
+++ src/Components/Rate/RatePost.js
@@ -1,42 +1,55 @@
 import React from 'react'
 import { Col,Row } from 'react-bootstrap';
 import ReactStars from "react-rating-stars-component";
+import AddRateHook from '../../CustomHook/rate/AddRateHook';
+import { useParams } from 'react-router-dom';
 
 const RatePost = () => {
+  const {id} = useParams();
+  const [rateText, onChangeRateText, rateValue, onChangeRateValue, user, onSubmit] = AddRateHook(id);
 
-    const setting = {
-        size: 20,
-        count: 5,
-        color: "#979797",
-        activeColor: "#ffc107",
-        value: 7.5,
-        a11y: true,
-        isHalf: true,
-        emptyIcon: <i className="far fa-star" />,
-        halfIcon: <i className="fa fa-star-half-alt" />,
-        filledIcon: <i className="fa fa-star" />,
-        onChange: newValue => {
-            console.log(`Example 2: new value is ${newValue}`);
-        }
-    };
+  let name;
+  if(user) {
+    name = user.name;
+  } else {
+    name = '';
+  }
+
+  const setting = {
+    size: 20,
+    count: 5,
+    color: "#979797",
+    activeColor: "#ffc107",
+    value: 7.5,
+    a11y: true,
+    isHalf: true,
+    emptyIcon: <i className="far fa-star" />,
+    halfIcon: <i className="fa fa-star-half-alt" />,
+    filledIcon: <i className="fa fa-star" />,
+    onChange: newValue => {
+      onChangeRateValue(newValue);
+    }
+  };
     return (
         <div>
         <Row className="mt-3 ">
           <Col sm="12" className="me-5  d-flex">
-            <div className="rate-name  d-inline ms-3 mt-1 ">علي محمد</div>
+            <div className="rate-name  d-inline ms-3 mt-1 ">{name}</div>
             <ReactStars {...setting} />
           </Col>
         </Row>
         <Row className="border-bottom mx-2">
           <Col className="d-felx me-4 pb-2">
             <textarea
+              value={rateText}
+              onChange={onChangeRateText}
               className="input-form-area p-2 mt-3"
               rows="2"
               cols="20"
               placeholder="اكتب تعليقك...."
             />
             <div className=" d-flex justify-content-end al">
-              <div className="product-cart-add px-3  py-2 text-center d-inline">اضف تعليق</div>
+              <div className="product-cart-add px-3  py-2 text-center d-inline" onClick={onSubmit}>اضف تعليق</div>
             </div>
           </Col>
         </Row>
