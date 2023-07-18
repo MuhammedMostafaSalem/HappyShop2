967bc9dbbd571cb3192e01f977dc685985de1117
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-18 17:43:28 +0300

    Add getAllRate in RateAction

--- src/Redux/Actions/RateAction.js
+++ src/Redux/Actions/RateAction.js
@@ -1,5 +1,6 @@
+import { useGetDataToken } from "../../Hooks/useGetData"
 import { useInsertData } from "../../Hooks/useInsertData"
-import { CREATE_RATE } from "../type"
+import { ALL_RATE_PRODUCT, CREATE_RATE } from "../type"
 
 //create rate
 export const createRate = (prodID, body) => async (dispatch) => {
@@ -13,6 +14,23 @@ export const createRate = (prodID, body) => async (dispatch) => {
     }catch(err) {
         dispatch({
             type: CREATE_RATE,
+            payload: err.response,
+        })
+    }
+}
+
+// get all review to one product 
+export const getAllRate = (prodID, page, limit) => async (dispatch) => {
+    try {
+        const response = await useGetDataToken(`/api/v1/products/${prodID}/reviews?page=${page}&limit=${limit}`)
+
+        dispatch({
+            type: ALL_RATE_PRODUCT,
+            payload: response,
+        })
+    }catch(err) {
+        dispatch({
+            type: ALL_RATE_PRODUCT,
             payload: err.response,
         })
     }
