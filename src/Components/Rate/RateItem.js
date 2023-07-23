40c9d4601f34084981960b707981b8f23a725aad
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-23 11:27:35 +0300

    Add EditRateHook in RateItem component

--- src/Components/Rate/RateItem.js
+++ src/Components/Rate/RateItem.js
@@ -4,10 +4,29 @@ import rate from '../../images/rate.png'
 import deleteIcon from '../../images/delete.png'
 import editIcon from '../../images/edit.png'
 import DeleteRateHook from '../../CustomHook/rate/DeleteRateHook'
+import EditRateHook from '../../CustomHook/rate/EditRateHook'
+import ReactStars from "react-rating-stars-component";
 const RateItem = ({review}) => {
     // console.log(review)
     // console.log(JSON.parse(localStorage.getItem('user')))
     const [isUser, handleClose, handleShow, handleDelete, showDelete]= DeleteRateHook(review);
+    const [showEdit, handleCloseEdit, handleEdit, handleShowEdit, editRateText, onChangeRateText, editRateValue, onChangeRateValue]= EditRateHook(review);
+
+    const setting = {
+        size: 20,
+        count: 5,
+        color: "#979797",
+        activeColor: "#ffc107",
+        value: editRateValue,
+        a11y: true,
+        isHalf: true,
+        emptyIcon: <i className="far fa-star" />,
+        halfIcon: <i className="fa fa-star-half-alt" />,
+        filledIcon: <i className="fa fa-star" />,
+        onChange: newValue => {
+            onChangeRateValue(newValue);
+        }
+    };
 
     return (
         <div>
@@ -25,6 +44,30 @@ const RateItem = ({review}) => {
                     </Button>
                 </Modal.Footer>
             </Modal>
+            
+            <Modal show={showEdit} onHide={handleCloseEdit}>
+                <Modal.Header>
+                    <Modal.Title>تعديل التقييم</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
+                    <ReactStars {...setting} />
+                    <input
+                        value={editRateText}
+                        onChange={onChangeRateText}
+                        type='text'
+                        className='w-100'
+                        style={{borderRadius: '20px', padding: '5px 20px'}}
+                    />
+                </Modal.Body>
+                <Modal.Footer style={{borderTop: "none"}}>
+                    <Button variant="danger" onClick={handleCloseEdit}>
+                        اغلاق
+                    </Button>
+                    <Button variant="secondary" onClick={handleEdit}>
+                        تعديل
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <Row className="mt-3">
                 <Col className="d-felx me-5">
                     <div className="rate-name  d-inline ms-2">{review.user.name}</div>
@@ -39,9 +82,9 @@ const RateItem = ({review}) => {
                     </div>
                     {
                         isUser === true ?
-                            <div class='d-flex gap-3'>
+                            <div className='d-flex gap-3'>
                                 <img src={deleteIcon} onClick={handleShow} width='20px' height='20px' style={{cursor: 'pointer'}} alt=''/>
-                                <img src={editIcon} width='20px' height='20px' style={{cursor: 'pointer'}} alt=''/>
+                                <img src={editIcon} onClick={handleShowEdit} width='20px' height='20px' style={{cursor: 'pointer'}} alt=''/>
                             </div>
                         : null
                     }
