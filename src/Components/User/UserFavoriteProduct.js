f62318ff95f076190a26de44e27043103537563a
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-27 15:38:34 +0300

    See the wishList in UserFavoriteProduct

--- src/Components/User/UserFavoriteProduct.js
+++ src/Components/User/UserFavoriteProduct.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row } from 'react-bootstrap';
 import ProductCard from './../Products/ProductCard';
 import Pagination from '../Uitily/Pagination'
+import { useDispatch, useSelector } from 'react-redux';
+import { getProductToWishList } from '../../Redux/Actions/WishListAction';
+import CardProductsContainer from '../Products/CardProductsContainer';
 const UserFavoriteProduct = () => {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true)
+    const [items, setItems] = useState([])
+    
+    useEffect(() => {
+        const get = async () => {
+            setLoading(true)
+            await dispatch(getProductToWishList())
+            setLoading(false)
+        }
+        get()
+    }, [])
+
+    const res = useSelector(state => state.WishListReducer.getUserWishList)
+
+    useEffect(() => {
+        if (loading === false) {
+            if (res) {
+                setItems(res.data)
+            }
+        }
+    }, [loading])
     return (
         <div>
-            <div className="admin-content-text pb-4">قائمة المفضلة</div>
+            <div className="admin-content-text">قائمة المفضلة</div>
             <Row className='justify-content-start'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                {
+                    items.length <= 0 ?
+                        <h6>لا يوجد منتجات مفضله الان</h6>
+                    : <CardProductsContainer products={items} title="" btntitle="" />
+                }
             </Row>
-            <Pagination />
         </div>
     )
 }
