77911affba230b93ab731c7352f7591c6035b24c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-29 15:04:11 +0300

    Add AddCouponHook in AdminAddCoupon

--- src/Components/Admin/AdminAddCoupon.js
+++ src/Components/Admin/AdminAddCoupon.js
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react'
 import { Col, Row } from 'react-bootstrap';
+import AddCouponHook from '../../CustomHook/Coupon/AddCouponHook';
 
 const AdminAddCoupon = () => {
+    const [couponName, couponDate, couponValue, onChangCouponName, onChangCouponDate, onChangCouponValue, onSubmit] = AddCouponHook();
     const dateRef = useRef();
 
     return (
@@ -13,13 +15,16 @@ const AdminAddCoupon = () => {
                         type="text"
                         className="input-form d-block mt-3 px-3"
                         placeholder="اسم الكوبون"
+                        value={couponName}
+                        onChange={onChangCouponName}
                     />
                     <input
                         type="text"
                         className="input-form d-block mt-3 px-3"
                         placeholder="تارخ الانتهاء"
                         ref={dateRef}
-                        onChange={(e) => console.log(e.target.value)}
+                        value={couponDate}
+                        onChange={onChangCouponDate}
                         onFocus={() => dateRef.current.type = 'date'}
                         onBlur={() => dateRef.current.type = 'text'}
                     />
@@ -27,12 +32,14 @@ const AdminAddCoupon = () => {
                         type="number"
                         className="input-form d-block mt-3 px-3"
                         placeholder="نسبة الخصم"
+                        value={couponValue}
+                        onChange={onChangCouponValue}
                     />
                 </Col>
             </Row>
             <Row>
                 <Col sm="8" className="d-flex justify-content-end">
-                    <button className="btn-save d-inline mt-2">حفظ</button>
+                    <button className="btn-save d-inline mt-2" onClick={onSubmit}>حفظ</button>
                 </Col>
             </Row>
         </div>
