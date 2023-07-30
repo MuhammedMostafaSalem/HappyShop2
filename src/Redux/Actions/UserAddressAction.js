5ed1fd2219db866fd3f52dc07bd7fd59b5d49eaf
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-30 17:35:34 +0300

    Add getOneUserAddress and editUserAddress in UserAddressAction

--- src/Redux/Actions/UserAddressAction.js
+++ src/Redux/Actions/UserAddressAction.js
@@ -1,6 +1,7 @@
+import { useEditData } from "../../Hooks/useEditData";
 import { useGetDataToken } from "../../Hooks/useGetData";
 import { useInsertData } from "../../Hooks/useInsertData";
-import { ADD_USER_ADDRESS, All_USER_ADDRESSES, DELET_USER_ADDRESS } from "../type";
+import { ADD_USER_ADDRESS, All_USER_ADDRESSES, DELET_USER_ADDRESS, EDIT_USER_ADDRESS, GET_ONE_ADDRESS } from "../type";
 import useDeleteData from './../../Hooks/useDeleteData';
 
 // create user Address
@@ -49,6 +50,40 @@ export const deleteUserAddress = (id) => async (dispatch) => {
     } catch (e) {
         dispatch({
             type: DELET_USER_ADDRESS,
+            payload: e.response,
+        })
+    }
+}
+
+// get one user Address
+export const getOneUserAddress = (id) => async (dispatch) => {
+    try {
+        const response = await useGetDataToken(`/api/v1/addresses/${id}`);
+        dispatch({
+            type: GET_ONE_ADDRESS,
+            payload: response,
+        })
+
+    } catch (e) {
+        dispatch({
+            type: GET_ONE_ADDRESS,
+            payload: e.response,
+        })
+    }
+}
+
+//edit user Address
+export const editUserAddress = (id, body) => async (dispatch) => {
+    try {
+        const response = await useEditData(`/api/v1/addresses/${id}`, body);
+        dispatch({
+            type: EDIT_USER_ADDRESS,
+            payload: response,
+        })
+
+    } catch (e) {
+        dispatch({
+            type: EDIT_USER_ADDRESS,
             payload: e.response,
         })
     }
