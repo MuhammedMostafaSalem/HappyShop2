f0a63e31d480ce18c4f2f25bd38261463fb1b089
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-07-30 17:36:39 +0300

    Add EditAddressHook and useParams in UserEditAddress Component

--- src/Components/User/UserEditAddress.js
+++ src/Components/User/UserEditAddress.js
@@ -1,36 +1,51 @@
 import React from 'react'
 import { Row,Col } from 'react-bootstrap'
+import { useParams } from 'react-router-dom';
+import EditAddressHook from '../../CustomHook/User/EditAddressHook';
 
 const UserEditAddress = () => {
+    const {id} = useParams();
+    const [alias, onChangAlias, detailsAddress, onChangDetailsAddress, phone, onChangPhone, city, onChangCity, onSubmit] = EditAddressHook(id)
+
     return (
         <div>
             <Row className="justify-content-start ">
                 <div className="admin-content-text pb-2">تعديل العنوان </div>
                 <Col sm="8">
                     <input
+                        value={alias}
+                        onChange={onChangAlias}
                         type="text"
                         className="input-form d-block mt-3 px-3"
-                        value="المنزل"
                         placeholder="تسمية العنوان مثلا(المنزل - العمل)"
                     />
                     <textarea
+                        value={detailsAddress}
+                        onChange={onChangDetailsAddress}
                         className="input-form-area p-2 mt-3"
                         rows="4"
                         cols="50"
-                        value="القاهرة ٦ اكتوبر"
                         placeholder="العنوان بالتفصيل"
                     />
                     <input
-                        type="text"
-                        value="01213621735"
+                        value={phone}
+                        onChange={onChangPhone}
+                        type="number"
                         className="input-form d-block mt-3 px-3"
                         placeholder="رقم الهاتف"
+                    />
+                    <input
+                        value={city}
+                        onChange={onChangCity}
+                        type="text"
+                        className="input-form d-block mt-3 px-3"
+                        placeholder="اسم المدينة"
                     />
                 </Col>
             </Row>
             <Row>
                 <Col sm="8" className="d-flex justify-content-end ">
-                    <button className="btn-save d-inline mt-2 ">حفظ تعديل العنوان</button>
+                    <button onClick={onSubmit} className="btn-save d-inline mt-2">تعديل</button>
                 </Col>
             </Row>
         </div>
