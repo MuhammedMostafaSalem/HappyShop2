12bc7faaa126008e7c7641eeb33de73b5e9b72f3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-08-09 08:10:43 +0300

    Add (AllUserCartHook, DeleteCartHook and ApplayCouponHook) in CartPage

--- src/Page/Cart/CartPage.js
+++ src/Page/Cart/CartPage.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import CartCheckout from '../../Components/Cart/CartCheckout'
 import CartItem from '../../Components/Cart/CartItem'
+import AllUserCartHook from '../../CustomHook/Cart/AllUserCartHook'
+import DeleteCartHook from './../../CustomHook/Cart/DeleteCartHook';
+import ApplayCouponHook from './../../CustomHook/Cart/ApplayCouponHook';
 
 const CartPage = () => {
+    const [itemsNums, cartItems, totalPriceCart, totalPriceAfterDiscount, couponNameRes] = AllUserCartHook()
+    const [deleteAllCartHandle] = DeleteCartHook();
+    const [couponName, onChangeCoupon, submitCouponHandel] = ApplayCouponHook();
+
     return (
         <Container style={{ minHeight: '670px' }}>
             <Row>
@@ -11,14 +18,19 @@ const CartPage = () => {
             </Row>
             <Row className='d-flex justify-content-center'>
                 <Col xs="12" md="9">
-                    <CartItem />
-                    <CartItem />
-                    <CartItem />
-                    <CartItem />
+                    {
+                        cartItems ?
+                            cartItems.map((item, index) => {
+                                return (
+                                    <CartItem key={index} item={item} />
+                                )
+                            })
+                        : <div className='text-center'>لا يوجد منتجات هنا</div>
+                    }
                 </Col>
 
                 <Col xs="6" md="3">
-                    <CartCheckout />
+                    <CartCheckout totalPriceCart={totalPriceCart} itemsNums={itemsNums} totalPriceAfterDiscount={totalPriceAfterDiscount}  couponNameRes={couponNameRes} deleteAllCartHandle={deleteAllCartHandle} couponName={couponName} onChangeCoupon={onChangeCoupon} submitCouponHandel={submitCouponHandel} />
                 </Col>
             </Row>
         </Container>
