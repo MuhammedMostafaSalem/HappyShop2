1eca4ee18ca20ea0aacf59390e23cf4dc0a8b7ea
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-08-11 14:38:50 +0300

    Add AllCategoryPageHook in CategoryHeader

--- src/Components/Category/CategoryHeader.js
+++ src/Components/Category/CategoryHeader.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container,Row ,Col} from 'react-bootstrap'
+import AllCategoryPageHook from './../../CustomHook/Category/AllCategoryPageHook';
+import { Link } from 'react-router-dom';
 
 const CategoryHeader = () => {
+  const [
+        categoryData,
+        loading,
+        pageCount,
+        getPage,
+    ] = AllCategoryPageHook()
+
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+      if(categoryData) {
+        setItems(categoryData.data)
+      }
+    }, [categoryData])
+
     return (
-        <div className="cat-header">
-      <Container>
-        <Row>
-          <Col className="d-flex justify-content-start py-2 flex-wrap">
-            <div className="cat-text-header ">الكل</div>
-            <div className="cat-text-header">الكترونيات</div>
-            <div className="cat-text-header">ملابس</div>
-            <div className="cat-text-header"> كهربيه</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">المزيد</div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+      <div className="cat-header">
+        <Container>
+          <Row>
+            <Col className="d-flex justify-content-start py-2 flex-wrap">
+              {
+                items ?
+                  items.map((item, index) => {
+                    return(
+                      <Link to={`/products/category/${item._id}`} style={{textDecoration: 'none'}}>
+                        <div className="cat-text-header" key={index}>{item.name}</div>
+                      </Link>
+                    )
+                  })
+                : null
+              }
+              <Link to='/allcategory' style={{textDecoration: 'none'}}>
+                <div className="cat-text-header">المزيد</div>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
 }
 
