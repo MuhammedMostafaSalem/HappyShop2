5b40cf60c83e25e59d00f5ca39091043561455a5
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-08-11 15:30:02 +0300

    Add id prop and link route in BrandCard Component

--- src/Components/Brand/BrandCard.js
+++ src/Components/Brand/BrandCard.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import { Col,Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
-const BrandCard = ({img}) => {
+const BrandCard = ({img, id}) => {
     return (
-        <Col
-        xs="6"
-        sm="6"
-        md="4"
-        lg="2"
-        className="my-2 d-flex justify-content-center">
-        <Card
-          className="my-1"
-          style={{
-            width: "100%",
-            height: "151px",
-            borderRadius: "8px",
-            border: "none",
-            backgroundColor: "#FFFFFF",
-          }}>
-          <Card.Img style={{ width: "100%", height: "151px" }} src={img} />
-        </Card>
+        <Col xs="6" sm="6" md="4" lg="2" className="my-2 d-flex justify-content-center">
+        <Link to={`/products/brand/${id}`} style={{textDecoration: 'none'}}>
+          <Card
+            className="my-1"
+            style={{ width: "100%", height: "151px", borderRadius: "8px", border: "none", backgroundColor: "#FFFFFF"}}
+          >
+            <Card.Img style={{ width: "100%", height: "151px" }} src={img} />
+          </Card>
+        </Link>
       </Col>
     )
 }
