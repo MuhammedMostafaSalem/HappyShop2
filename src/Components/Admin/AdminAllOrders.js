262237d9e096dcb67539106677f412b1bc089ec4
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-08-13 16:59:57 +0300

    Add UserGetAllOrderHook in AdminAllOrders

--- src/Components/Admin/AdminAllOrders.js
+++ src/Components/Admin/AdminAllOrders.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import { Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 import AdminAllOrdersItem from './AdminAllOrdersItem'
+import UserGetAllOrderHook from '../../CustomHook/User/UserGetAllOrderHook'
+import Pagination from '../Uitily/Pagination'
 
 const AdminAllOrders = () => {
+    const [userName, results, paginate, orderData, onPress] = UserGetAllOrderHook()
+
     return (
         <div>
             <div className='admin-content-text'>ادارة جميع الطلبات</div>
             <Row className='justify-content-start'>
-                <AdminAllOrdersItem />
-                <AdminAllOrdersItem />
-                <AdminAllOrdersItem />
+                {
+                    orderData.length >= 1 ?
+                        orderData.map((orderItem, index) => {
+                            return (
+                                <AdminAllOrdersItem key={index} orderItem={orderItem} />
+                            )
+                        })
+                    : <h6>لا يوجد طلبات حتى الان</h6>
+                }
+            </Row>
+            <Row>
+                <Col>
+                    {
+                        paginate.numberOfPages > 2 ? <Pagination pageCount={paginate.numberOfPages} onPress={onPress} /> : null
+                    }
+                </Col>
             </Row>
         </div>
     )
