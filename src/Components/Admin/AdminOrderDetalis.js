d857a68a2f74174f21b82b0833e3babd18ba900b
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-08-13 16:59:09 +0300

    Add GetOrderDetalisHook  and ChangeOrderStatusHook in AdminOrderDetalis

--- src/Components/Admin/AdminOrderDetalis.js
+++ src/Components/Admin/AdminOrderDetalis.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Row,Col } from 'react-bootstrap'
-import CartItem from '../Cart/CartItem'
+import UserAllOrderItem from './../User/UserAllOrderItem';
+import { useParams } from 'react-router-dom';
+import GetOrderDetalisHook from '../../CustomHook/Admin/GetOrderDetalisHook';
+import ChangeOrderStatusHook from '../../CustomHook/Admin/ChangeOrderStatusHook';
 
 const AdminOrderDetalis = () => {
+    const { id } = useParams()
+    const [orderData, cartItems] = GetOrderDetalisHook(id)
+    const [onChangePaid, onChangeDeliver, changePayOrder, changeDeliverOrder, formatDate] = ChangeOrderStatusHook(id)
+
     return (
         <div>
-            <div className='admin-content-text'>تفاصيل الطلب رقم#55</div>
-            <CartItem />
-            <CartItem />
-            <CartItem />
+            <UserAllOrderItem orderItem={orderData} />
 
-            <Row className="justify-content-center mt-4 user-data">
+            <Row className="justify-content-center mt-4 p-3 user-data">
                 <Col xs="12" className=" d-flex">
-                    <div className="admin-content-text py-2">تفاصيل العميل</div>
+                    <div className="admin-content-text pb-2">تفاصيل العميل</div>
                 </Col>
                 <Col xs="12" className="d-flex">
                     <div
@@ -31,7 +35,11 @@ const AdminOrderDetalis = () => {
                             fontSize: "16px",
                         }}
                         className="mx-2">
-                        احمد عبداللة
+                        {
+                            orderData ?
+                                orderData.user ? orderData.user.name : null
+                            : null
+                        }
                     </div>
                 </Col>
 
@@ -52,7 +60,11 @@ const AdminOrderDetalis = () => {
                             fontSize: "16px",
                         }}
                         className="mx-2">
-                        0021313432423
+                        {
+                            orderData ?
+                                orderData.user ? orderData.user.phone : null
+                            : null
+                        }
                     </div>
                 </Col>
                 <Col xs="12" className="d-flex">
@@ -72,23 +84,39 @@ const AdminOrderDetalis = () => {
                             fontSize: "16px",
                         }}
                         className="mx-2">
-                        ahmed@gmail.com
+                        {
+                            orderData ?
+                                orderData.user ? orderData.user.email : null
+                            : null
+                        }
                     </div>
                 </Col>
-                <div className=" d-inline px-4 border text-center pt-2">
-                    المجموع ٤٠٠٠ جنيه
-                </div>
                 <div className="d-flex mt-2 justify-content-center">
                     <select
                         name="languages"
                         id="lang"
-                        className="select input-form-area mt-1  text-center px-2 w-50">
-                        <option value="val">حالة الطلب</option>
-                        <option value="val2">قيد التنفيذ</option>
-                        <option value="val2">تم الانتهاء</option>
-                        <option value="val2">الغاء</option>
+                        className="select input-form-area mt-1  text-center px-2 w-50"
+                        onChange={onChangePaid}
+                    >
+                        <option value="val">الدفع</option>
+                        <option value="true">تم</option>
+                        <option value="false">لم يتم</option>
                     </select>
-                    <button className="btn-a px-3 d-inline mx-2 ">حفظ </button>
+                    <button onClick={changePayOrder} className="btn-a px-3 d-inline mx-2">حفظ </button>
+                </div>
+
+                <div className="d-flex mt-2 justify-content-center">
+                    <select
+                        name="languages"
+                        id="lang"
+                        className="select input-form-area mt-1  text-center px-2 w-50"
+                        onChange={onChangeDeliver}
+                    >
+                        <option value="val">التوصيل</option>
+                        <option value="true">تم</option>
+                        <option value="false">لم يتم</option>
+                    </select>
+                    <button onClick={changeDeliverOrder} className="btn-a px-3 d-inline mx-2">حفظ </button>
                 </div>
             </Row>
         </div>
